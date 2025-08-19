5b266945be64eede81ceb76910302f71c5feef69
Chiman2937 <cyga2933@gmail.com>
2025-08-19 18:27:19 +0900

    build: next.config.ts에 svgr webpack 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,16 @@
+// next.config.ts
 import type { NextConfig } from 'next';
+import type { Configuration as WebpackConfig } from 'webpack';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack(config: WebpackConfig) {
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
