d0dc1e5a9c675a2e6aaaba38d848e1e9f429c772
Chiman2937 <cyga2933@gmail.com>
2025-08-20 13:06:06 +0900

    feat: twMerge와 clsx를 조합한 cn.tsx 유틸 생성

---
+++ src/lib/cn.ts
@@ -0,0 +1,7 @@
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+// 조건부 class 처리 + Tailwind 중복 class 정리
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
