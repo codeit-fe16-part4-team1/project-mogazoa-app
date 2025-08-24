535956d1550692ac25b38288f88b006afa1b5de2
Chiman2937 <cyga2933@gmail.com>
2025-08-24 14:28:03 +0900

    refactor: getQueryClient 별도 파일로 분리(getQueryClient.ts)

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,28 +1,8 @@
 'use client';
 import useUserInitialize from '@/hooks/useUserInitialize';
-import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { getQueryClient } from '@/lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-const makeQueryClient = () => {
-  return new QueryClient({
-    defaultOptions: {
-      queries: {
-        staleTime: 60 * 1000,
-      },
-    },
-  });
-};
-
-let browserQueryClient: QueryClient | undefined = undefined;
-
-const getQueryClient = () => {
-  if (isServer) {
-    return makeQueryClient();
-  } else {
-    if (!browserQueryClient) browserQueryClient = makeQueryClient();
-    return browserQueryClient;
-  }
-};
 
 interface Props {
   children: React.ReactNode;
