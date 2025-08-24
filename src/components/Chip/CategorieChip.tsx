6522ae41732184402d10f3489b1ccb8605b3e4bd
Heedong <donghui11777@gmail.com>
2025-08-24 20:59:46 +0900

    fix: CategorieChip 기본 텍스트 스타일 변경

--- src/components/Chip/CategorieChip.tsx
+++ src/components/Chip/CategorieChip.tsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/cn';
 import { generateColorsFromText } from '@/utils/generateColorsFromText ';
 
 const categorieChipVariants = cva(
-  'inline-flex items-center justify-center rounded-md border px-2 py-1 caption-medium text-xs font-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
+  'inline-flex items-center justify-center rounded-md border px-2 py-1 caption-medium w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive transition-[color,box-shadow] overflow-hidden',
   {
     variants: {
       variant: {
