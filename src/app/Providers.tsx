8b5cea68b6fb0cbe120bbee84adf183a128522e6
Chiman2937 <cyga2933@gmail.com>
2025-08-24 14:22:51 +0900

    refactor: react query Provider - tanstack query v5 권장사항으로 코드 변경

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,15 +1,35 @@
 'use client';
 import useUserInitialize from '@/hooks/useUserInitialize';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { isServer, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { useState } from 'react';
+
+const makeQueryClient = () => {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 60 * 1000,
+      },
+    },
+  });
+};
+
+let browserQueryClient: QueryClient | undefined = undefined;
+
+const getQueryClient = () => {
+  if (isServer) {
+    return makeQueryClient();
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient();
+    return browserQueryClient;
+  }
+};
 
 interface Props {
   children: React.ReactNode;
 }
 
 export const Providers = ({ children }: Props) => {
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = getQueryClient();
   useUserInitialize();
   return (
     <QueryClientProvider client={queryClient}>
