bb4a74a1b8e0bd47c50cd6ab0bb96c4c42bb135d
Chiman2937 <cyga2933@gmail.com>
2025-08-26 15:17:07 +0900

    feat: 회원가입/로그인 페이지에 FormField 디자인 적용

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 import FormField from '@/components/FormField/FormField';
-import FormHint from '@/components/FormField/FormHint';
-import FormInput from '@/components/FormField/FormInput';
-import FormLabel from '@/components/FormField/FormLabel';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useForm } from 'react-hook-form';
 import z from 'zod';
@@ -14,25 +11,25 @@ const signupSchema = z
   .object({
     email: z
       .string()
-      .min(1, '이메일은 필수 입력입니다.') //빈칸 에러메시지
-      .email('올바른 이메일 형식을 입력하세요.'), //이메일 형식 검사
+      .min(1, '이메일을 입력해주세요') //빈칸 에러메시지
+      .email('이메일 형식으로 작성해 주세요'), //이메일 형식 검사
     nickname: z
       .string()
-      .min(1, '닉네임은 필수 입력입니다.') //빈칸 에러메시지
-      .max(20, '닉네임은 최대 20자까지 가능합니다.'), //닉네임 형식 검사
+      .min(1, '닉네임을 입력해주세요') //빈칸 에러메시지
+      .max(10, '닉네임은 최대 10자까지 가능해요.'), //닉네임 형식 검사
     password: z
       .string()
-      .min(1, '비밀번호는 필수 입력입니다.') //빈칸 에러메시지
-      .min(8, '비밀번호는 최소 8자 이상입니다.') //비밀번호 형식 검사
-      .regex(/^[a-zA-Z0-9!@#$%^&*]+$/, '비밀번호는 숫자, 영문, 특수문자로만 가능합니다.'),
+      .min(1, '비밀번호를 입력해주세요') //빈칸 에러메시지
+      .min(8, '비밀번호는 8자 이상 입력해주세요') //비밀번호 형식 검사
+      .regex(/^[a-zA-Z0-9!@#$%^&*]+$/, '비밀번호는 숫자, 영문, 특수문자로만 가능해요'),
     passwordConfirmation: z
       .string()
       .min(1, '비밀번호는 필수 입력입니다.') //빈칸 에러메시지
-      .min(8, '비밀번호는 최소 8자 이상입니다.') //비밀번호 확인 형식 검사
-      .regex(/^[a-zA-Z0-9!@#$%^&*]+$/, '비밀번호는 숫자, 영문, 특수문자로만 가능합니다.'),
+      .min(8, '비밀번호는 8자 이상 입력해주세요') //비밀번호 확인 형식 검사
+      .regex(/^[a-zA-Z0-9!@#$%^&*]+$/, '비밀번호는 숫자, 영문, 특수문자로만 가능해요'),
   })
   .refine((data) => data.password === data.passwordConfirmation, {
-    message: '비밀번호가 일치하지 않습니다.',
+    message: '비밀번호가 일치하지 않아요',
     path: ['passwordConfirmation'],
   });
 
@@ -72,60 +69,49 @@ const SignupPage = () => {
       className='mx-auto max-w-sm space-y-4 rounded-md border p-4 shadow-sm'
       onSubmit={handleSubmit(onSubmit)}
     >
-      {/* 이메일 입력 */}
-      <FormField>
-        <FormLabel htmlFor='email'>이메일</FormLabel>
-        <FormInput
-          id='email'
-          type='email'
-          placeholder='이메일을 입력해 주세요'
-          autoComplete='email'
-          register={register('email')}
-        />
-        <FormHint>{errors.email?.message}</FormHint>
-      </FormField>
-      {/* 닉네임 입력 */}
-      <FormField>
-        <FormLabel htmlFor='nickname'>닉네임</FormLabel>
-        <FormInput
-          id='nickname'
-          type='text'
-          placeholder='닉네임을 입력해 주세요'
-          register={register('nickname')}
-        />
-        <FormHint>{errors.nickname?.message}</FormHint>
-      </FormField>
-      {/* 비밀번호 입력 */}
-      <FormField>
-        <FormLabel htmlFor='password'>비밀번호</FormLabel>
-        <FormInput
-          id='password'
-          type='password'
-          placeholder='비밀번호를 입력해 주세요'
-          autoComplete='new-password'
-          register={register('password', {
-            onChange: () => {
-              const passwordConfirm = getValues('passwordConfirmation');
-              if (passwordConfirm) {
-                trigger('passwordConfirmation');
-              }
-            },
-          })}
-        />
-        <FormHint>{errors.password?.message}</FormHint>
-      </FormField>
-      {/* 비밀번호 확인 입력 */}
-      <FormField>
-        <FormLabel htmlFor='passwordConfirm'>비밀번호 확인</FormLabel>
-        <FormInput
-          id='passwordConfirm'
-          type='password'
-          placeholder='비밀번호를 다시 한 번 입력해 주세요'
-          autoComplete='new-password'
-          register={register('passwordConfirmation')}
-        />
-        <FormHint>{errors.passwordConfirmation?.message}</FormHint>
-      </FormField>
+      <FormField
+        label='이메일'
+        id='email'
+        type='email'
+        placeholder='이메일을 입력해 주세요'
+        autoComplete='email'
+        register={register('email')}
+        error={errors.email}
+      />
+      <FormField
+        label='닉네임'
+        id='nickname'
+        type='text'
+        placeholder='닉네임을 입력해 주세요'
+        autoComplete='email'
+        register={register('nickname')}
+        error={errors.nickname}
+      />
+      <FormField
+        label='비밀번호'
+        id='password'
+        type='password'
+        placeholder='비밀번호를 입력해 주세요'
+        autoComplete='new-password'
+        register={register('password', {
+          onChange: () => {
+            const passwordConfirm = getValues('passwordConfirmation');
+            if (passwordConfirm) {
+              trigger('passwordConfirmation');
+            }
+          },
+        })}
+        error={errors.password}
+      />
+      <FormField
+        label='비밀번호 확인'
+        id='passwordConfirm'
+        type='password'
+        placeholder='비밀번호를 다시 한 번 입력해 주세요'
+        autoComplete='new-password'
+        register={register('passwordConfirmation')}
+        error={errors.passwordConfirmation}
+      />
 
       {/* 제출 버튼 */}
       <button
@@ -133,7 +119,7 @@ const SignupPage = () => {
         disabled={isSubmitting || !isValid}
         className='w-full rounded-md bg-blue-500 p-2 text-white hover:bg-blue-600 disabled:bg-gray-400'
       >
-        {isSubmitting ? '회원가입 중...' : '회원가입'}
+        {isSubmitting ? '가입 중...' : '가입하기'}
       </button>
     </form>
   );
