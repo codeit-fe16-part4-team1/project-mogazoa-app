71f2063e41b1988ec27bc0807bd539e1760ef188
dain <gunmannduu@gmail.com>
2025-08-27 15:07:25 +0900

    chore: page.tsx 비움

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,5 @@
-import Dropdown from '@/components/Dropdown/Dropdown';
-import DropdownItem from '@/components/Dropdown/DropdownItem';
-import DropdownList from '@/components/Dropdown/DropdownList';
-
 const Home = () => {
-  return (
-    <div className='flex justify-center p-10'>
-      <Dropdown placeholder='카테고리 선택' size='mq'>
-        <DropdownList>
-          <DropdownItem label='음악' />
-          <DropdownItem label='운동' />
-          <DropdownItem label='게임' />
-          <DropdownItem label='여행' />
-          <DropdownItem label='스터디' />
-        </DropdownList>
-      </Dropdown>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
