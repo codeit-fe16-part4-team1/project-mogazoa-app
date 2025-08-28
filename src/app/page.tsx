6223ec944bea52182a82ac5bbb16beb9f3e9d5cc
dain <gunmannduu@gmail.com>
2025-08-28 15:42:08 +0900

    chore: page.tsx정리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-'use client';
-
-import React, { useEffect, useState } from 'react';
-import Sort from '@/components/Sort/Sort';
-
 const Home = () => {
-  const [selectedSort, setSelectedSort] = useState('최신순');
-
-  useEffect(() => {
-    console.log('Selected Sort:', selectedSort);
-  }, [selectedSort]);
-
-  return (
-    <div>
-      <Sort size='mq' value={selectedSort} onChange={setSelectedSort} />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
