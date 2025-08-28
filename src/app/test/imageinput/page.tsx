4057a646280d2e5bdfe25dd663d158ca54f2576f
Chiman2937 <cyga2933@gmail.com>
2025-08-28 10:24:18 +0900

    feat: ImageInput Test 페이지 제작

--- src/app/test/imageinput/page.tsx
+++ src/app/test/imageinput/page.tsx
@@ -2,38 +2,50 @@
 import { Controller, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
-import ImageInput, { ImageInputSchema } from '@/components/ImageInput/ImageInput';
+import ImageInput, {
+  getInitialValue,
+  getUploadedImageList,
+  ImageInputSchema,
+} from '@/components/ImageInput/ImageInput';
 
 const schema = z.object({
-  images: ImageInputSchema,
+  images: ImageInputSchema(3),
 });
 type FormData = z.infer<typeof schema>;
 
-const FileUploadForm = () => {
+export default function FileUploadForm() {
   const {
     control,
+    getValues,
     formState: { isValid },
   } = useForm<FormData>({
     resolver: zodResolver(schema),
     mode: 'onChange',
     defaultValues: {
-      images: [],
+      images: getInitialValue([
+        'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Mogazoa/user/832/1756312692708/image.png',
+      ]),
     },
   });
 
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const list = await getUploadedImageList(getValues('images'));
+    console.log(list);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <Controller
         name='images'
         control={control}
-        render={({ field }) => <ImageInput value={field.value} onChange={field.onChange} />}
-      />
-
+        render={({ field }) => (
+          <ImageInput value={field.value} onChange={field.onChange} maxImageCount={3} />
+        )}
+      ></Controller>
       <button type='submit' disabled={!isValid}>
         업로드 {isValid ? '✓' : '✗'}
       </button>
     </form>
   );
-};
-
-export default FileUploadForm;
+}
