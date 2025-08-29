58d20c772f8a24fe76b1d53c84eba3dff60aaa7b
qkrekdls <gunmannduu@gmail.com>
2025-08-29 15:49:31 +0900

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
+import CompareBar from '@/components/CompareBar/CompareBar';
+import { ProductItem } from '@/types/api';
+
 const Home = () => {
-  return <div></div>;
+  const handleProductSelection = (selectedProduct: ProductItem) => {
+    console.log('선택된 상품: ', selectedProduct.name);
+  };
+
+  return (
+    <div>
+      <h1>상품 비교 페이지</h1>
+      <CompareBar onSelectProduct={handleProductSelection} />
+    </div>
+  );
 };
 
 export default Home;
