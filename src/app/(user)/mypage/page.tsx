b22cefddb07a74cc04957d2bf1391bae0a2bbf74
Chiman2937 <cyga2933@gmail.com>
2025-08-30 21:17:05 +0900

    fix: mockData 별도 파일로 분리

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -1,28 +1,7 @@
 import ProfileSection from '../components/ProfileSection';
-import { Profile } from '@/types/api';
+import { mockProfile } from '../mock/profile';
 
 const MyPage = async () => {
-  // const profile = await getMyProfileAPI();
-  const mockProfile: Profile = {
-    updatedAt: '2025-08-30T10:15:00Z',
-    createdAt: '2024-12-01T09:00:00Z',
-    teamId: '16-1-1',
-    image:
-      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Mogazoa/user/832/1756526130023/image.png',
-    description: '프론트엔드 개발을 좋아하는 열정적인 개발자입니다.',
-    nickname: '치영',
-    id: 832,
-    mostFavoriteCategory: {
-      name: '가구/인테리어',
-      id: 101,
-    },
-    averageRating: 4.8,
-    reviewCount: 25,
-    followeesCount: 120,
-    followersCount: 98,
-    isFollowing: false,
-  };
-
   return (
     <div className='bg-gray-100'>
       <ProfileSection profile={mockProfile} />
