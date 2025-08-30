da8186c7d95ab12aac2cf0a5b57f684b1fa57b0f
dain <gunmannduu@gmail.com>
2025-08-30 18:16:41 +0900

    fix: padding값 수정

--- src/app/dropdowntest/page.tsx
+++ src/app/dropdowntest/page.tsx
@@ -6,9 +6,10 @@ import { useState } from 'react';
 
 const DropdownTestPage = () => {
   const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+  const [selectedSort, setSelectedSort] = useState<string | null>('최신순');
 
   return (
-    <div>
+    <div className='m-5 flex flex-col gap-50'>
       <Dropdown
         initialValue={selectedCategory}
         onChange={setSelectedCategory}
@@ -19,6 +20,13 @@ const DropdownTestPage = () => {
         <DropdownItem label='영화' />
         <DropdownItem label='드라마' />
         <DropdownItem label='연극' />
+      </Dropdown>
+
+      <Dropdown initialValue={selectedSort} onChange={setSelectedSort} size='S'>
+        <DropdownItem label='최신순' />
+        <DropdownItem label='별점 높은순' />
+        <DropdownItem label='별점 낮은순' />
+        <DropdownItem label='좋아요순' />
       </Dropdown>
     </div>
   );
