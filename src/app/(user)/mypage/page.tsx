6d03c61ea1908fa00fb935977f263d0a257ddfb6
Chiman2937 <cyga2933@gmail.com>
2025-08-30 21:07:07 +0900

    feat: 유저 프로필 화면 - 프로필 영역 UI 구현

---
+++ src/app/(user)/mypage/page.tsx
@@ -0,0 +1,33 @@
+import ProfileSection from '../components/ProfileSection';
+import { Profile } from '@/types/api';
+
+const MyPage = async () => {
+  // const profile = await getMyProfileAPI();
+  const mockProfile: Profile = {
+    updatedAt: '2025-08-30T10:15:00Z',
+    createdAt: '2024-12-01T09:00:00Z',
+    teamId: '16-1-1',
+    image:
+      'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Mogazoa/user/832/1756526130023/image.png',
+    description: '프론트엔드 개발을 좋아하는 열정적인 개발자입니다.',
+    nickname: '치영',
+    id: 832,
+    mostFavoriteCategory: {
+      name: '가구/인테리어',
+      id: 101,
+    },
+    averageRating: 4.8,
+    reviewCount: 25,
+    followeesCount: 120,
+    followersCount: 98,
+    isFollowing: false,
+  };
+
+  return (
+    <div className='bg-gray-100'>
+      <ProfileSection profile={mockProfile} />
+    </div>
+  );
+};
+
+export default MyPage;
