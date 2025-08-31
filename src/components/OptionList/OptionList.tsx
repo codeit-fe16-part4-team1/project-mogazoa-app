bb314aadf67a8f050a5c30527c83ae2c04a161bd
Chiman2937 <cyga2933@gmail.com>
2025-09-01 01:18:35 +0900

    fix: OptionList 컴포넌트 Headless Ui 패턴 적용

--- src/components/OptionList/OptionList.tsx
+++ src/components/OptionList/OptionList.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { createContext, useContext } from 'react';
+import { createContext, HTMLAttributes, useContext } from 'react';
 
 interface OptionContextType {
   selectedValue?: string;
@@ -18,32 +18,37 @@ interface ProviderProps {
 const OptionList = ({ children, selectedValue, className }: ProviderProps) => {
   return (
     <OptionContext.Provider value={{ selectedValue }}>
-      <div className={clsx('flex flex-row', className)}>{children}</div>
+      <div className={className}>{children}</div>
     </OptionContext.Provider>
   );
 };
 
-interface ButtonProps {
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
+  className?: string;
+  activeStyle?: string;
+  inactiveStyle?: string;
   value: string;
   onClick: () => void;
 }
 
-const OptionButton = ({ children, value, onClick }: ButtonProps) => {
+const OptionButton = ({
+  children,
+  className,
+  activeStyle,
+  inactiveStyle,
+  value,
+  onClick,
+  ...rest
+}: ButtonProps) => {
   const { selectedValue } = useContext(OptionContext);
   const isSelected = value === selectedValue;
   return (
     <button
-      className={clsx(
-        'text-sub-headline-medium flex items-center justify-center',
-        'h-14 w-40',
-        'cursor-pointer',
-        isSelected
-          ? 'border-b-3 border-gray-900 text-gray-800'
-          : 'border-b-1 border-gray-400 text-gray-400',
-        !isSelected && 'hover:bg-gray-200',
-      )}
+      data-state={isSelected ? 'active' : 'inactive'}
+      className={clsx(className, activeStyle, inactiveStyle)}
       onClick={onClick}
+      {...rest}
     >
       {children}
     </button>
