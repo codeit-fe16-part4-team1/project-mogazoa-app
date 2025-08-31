eb38bbf230912c2bc3121a62c873f064f1333387
Chiman2937 <cyga2933@gmail.com>
2025-08-31 09:34:28 +0900

    fix: OptionList 컴포넌트 context 적용

--- src/components/OptionButton/OptionList.tsx
+++ src/components/OptionButton/OptionList.tsx
@@ -1,20 +1,45 @@
-interface ProviderProps {
-  children: React.ReactNode;
+import clsx from 'clsx';
+import { createContext, useContext } from 'react';
+
+interface OptionContextType {
+  selectedValue?: string;
 }
 
-const OptionList = ({ children }: ProviderProps) => {
-  return <div className='flex flex-row'>{children}</div>;
+const OptionContext = createContext<OptionContextType>({
+  selectedValue: '',
+});
+
+interface ProviderProps {
+  children: React.ReactNode;
+  selectedValue?: string;
+  className?: string;
+}
+
+const OptionList = ({ children, selectedValue, className }: ProviderProps) => {
+  return (
+    <OptionContext.Provider value={{ selectedValue }}>
+      <div className={clsx('flex flex-row', className)}>{children}</div>
+    </OptionContext.Provider>
+  );
 };
 
 interface ButtonProps {
   children: React.ReactNode;
-  onClick?: () => void;
+  value: string;
+  onClick: () => void;
 }
 
-const OptionButton = ({ children, onClick }: ButtonProps) => {
+const OptionButton = ({ children, value, onClick }: ButtonProps) => {
+  const { selectedValue } = useContext(OptionContext);
+  const isSelected = value === selectedValue;
   return (
     <button
-      className='text-sub-headline flex h-14 w-40 items-center justify-center text-gray-800'
+      className={clsx(
+        'text-sub-headline-medium flex h-14 w-40 items-center justify-center',
+        isSelected
+          ? 'border-b-3 border-gray-900 text-gray-800'
+          : 'border-b-1 border-gray-400 text-gray-400',
+      )}
       onClick={onClick}
     >
       {children}
