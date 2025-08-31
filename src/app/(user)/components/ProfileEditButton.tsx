97ca05ac88ac07c753f64797f24c149e7688dbfd
Kim Chiyoung <cyga2933@gmail.com>
2025-08-31 12:19:17 +0900

    fix: ProfileEditButton Props를 HTML요소에서 상속받아오도록 수정

--- src/app/(user)/components/ProfileEditButton.tsx
+++ src/app/(user)/components/ProfileEditButton.tsx
@@ -1,18 +1,18 @@
 import clsx from 'clsx';
+import { HTMLAttributes } from 'react';
 
-interface Props {
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+  className?: string;
   onClick?: () => void;
 }
 
-const ProfileEditButton = ({ onClick }: Props) => {
+const ProfileEditButton = ({ className, onClick }: Props) => {
   return (
     <button
       className={clsx(
         'bg-gray-150 rounded-x1 text-gray-700 hover:bg-gray-300',
-        'text-caption-medium md:text-body1-medium absolute',
-        'px-3 py-2 md:px-5 md:py-3',
-        'top-0 right-0 bottom-auto',
-        'md:top-auto md:right-7 md:bottom-0',
+        'text-caption-medium md:text-body1-medium',
+        className,
       )}
       onClick={onClick}
     >
