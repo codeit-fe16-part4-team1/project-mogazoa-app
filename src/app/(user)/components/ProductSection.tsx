fc54b1e500d6e008a21b64cb533629fe2980e3bc
Chiman2937 <cyga2933@gmail.com>
2025-09-01 01:19:15 +0900

    fix: ProductSection에서 OptionList 컴포넌트 스타일 지정

--- src/app/(user)/components/ProductSection.tsx
+++ src/app/(user)/components/ProductSection.tsx
@@ -4,6 +4,7 @@ import OptionList from '@/components/OptionList/OptionList';
 import ProductCard from '@/components/ProductCard/ProductCard';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { getUserProductsAPI, ProductType } from '@/api/user/getUserProductsAPI';
+import clsx from 'clsx';
 
 interface Props {
   id: number;
@@ -29,10 +30,18 @@ const ProductSection = ({ id }: Props) => {
   return (
     <section className='px-4 pt-6 pb-11 md:px-15 md:pt-9 md:pb-18'>
       <div className='mx-auto max-w-235'>
-        <OptionList className='mb-8' selectedValue={productType}>
+        <OptionList className='mb-8 flex flex-row' selectedValue={productType}>
           {Object.entries(OPTION_MAP).map(([value, label]) => (
             <OptionList.button
               key={value}
+              role='tab'
+              className={clsx(
+                'text-sub-headline-medium flex items-center justify-center',
+                'h-14 w-40',
+                'cursor-pointer',
+                'data-[state=active]:border-b-3 data-[state=active]:border-gray-900 data-[state=active]:text-gray-800',
+                'data-[state=inactive]:border-b-1 data-[state=inactive]:border-gray-400 data-[state=inactive]:text-gray-400 data-[state=inactive]:hover:bg-gray-200',
+              )}
               value={value}
               onClick={() => setProductType(value as ProductType)}
             >
