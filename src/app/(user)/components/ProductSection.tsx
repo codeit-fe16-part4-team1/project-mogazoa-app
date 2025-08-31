2012b2e8b82f4844bc087aeb812a322fab370e43
Kim Chiyoung <cyga2933@gmail.com>
2025-08-31 12:58:05 +0900

    feat: 유저페이지 상품목록 UI 구현 완료

--- src/app/(user)/components/ProductSection.tsx
+++ src/app/(user)/components/ProductSection.tsx
@@ -2,7 +2,7 @@
 import { useState } from 'react';
 import { mockProductsList } from '../mock/product';
 import ProductCard from '@/components/ProductCard/ProductCard';
-import OptionList from '@/components/OptionButton/OptionList';
+import OptionList from '@/components/OptionList/OptionList';
 
 const ProductSection = () => {
   const [productType, setProductType] = useState('created');
@@ -15,14 +15,15 @@ const ProductSection = () => {
 
   return (
     <section className='px-4 pt-6 pb-11 md:px-15 md:pt-9 md:pb-18'>
-      <OptionList className='mb-8' selectedValue={productType}>
-        {Object.entries(OPTION_MAP).map(([value, label]) => (
-          <OptionList.button key={value} value={value} onClick={() => setProductType(value)}>
-            {label}
-          </OptionList.button>
-        ))}
-      </OptionList>
-
+      <div className='mx-auto max-w-235'>
+        <OptionList className='mb-8' selectedValue={productType}>
+          {Object.entries(OPTION_MAP).map(([value, label]) => (
+            <OptionList.button key={value} value={value} onClick={() => setProductType(value)}>
+              {label}
+            </OptionList.button>
+          ))}
+        </OptionList>
+      </div>
       <div className='mx-auto grid max-w-235 grid-cols-2 gap-x-3 gap-y-8 md:gap-x-5 md:gap-y-12 lg:grid-cols-3'>
         {mockProductsList.list.map((product) => (
           <ProductCard
