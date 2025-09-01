648ef2830d45579e5d81606490bb0f644deddd23
dain <gunmannduu@gmail.com>
2025-09-01 16:19:05 +0900

    feat: 승패 하이라이트 디자인 적용

--- src/components/CompareDetail/CompareDetail.tsx
+++ src/components/CompareDetail/CompareDetail.tsx
@@ -22,31 +22,36 @@ const CompareDetail = ({
     <div className='rounded-x4 flex w-full flex-col items-center justify-center gap-6 bg-white'>
       {/* 별점 */}
       <div className='flex w-[234px] items-center justify-between border-b border-gray-200 py-5 text-center'>
-        {isRatingWinner ? spacer : spacer}
-        <span
-          className={`text-h2-bold relative inline-flex items-center justify-center ${isRatingWinner ? 'h-6 rounded-full bg-orange-200 py-5' : ''}`}
-        >
-          {rating.toFixed(1)}
+        {spacer}
+        <span className='relative inline-flex items-center justify-center'>
+          {isRatingWinner && (
+            <span className='absolute top-1/4 right-0 left-0 z-0 h-6 rounded-full bg-orange-200'></span>
+          )}
+          <span className='text-h2-bold relative z-10 px-4'>{rating.toFixed(1)}</span>
         </span>
         {isRatingWinner ? winIcon : spacer}
       </div>
+
       {/* 리뷰 */}
       <div className='flex w-[234px] items-center justify-between border-b border-gray-200 pb-5 text-center'>
-        {isRatingWinner ? spacer : spacer}
-        <span
-          className={`text-h2-bold relative inline-flex items-center justify-center ${isReviewCountWinner ? 'h-6 rounded-full bg-orange-200 py-5' : ''}`}
-        >
-          {reviewCount}개
+        {spacer}
+        <span className='relative inline-flex items-center justify-center'>
+          {isReviewCountWinner && (
+            <span className='absolute top-1/4 right-0 left-0 z-0 h-6 rounded-full bg-orange-200'></span>
+          )}
+          <span className='text-h2-bold relative z-10 px-4'>{reviewCount}개</span>
         </span>
         {isReviewCountWinner ? winIcon : spacer}
       </div>
+
       {/* 좋아요 */}
       <div className='flex w-[234px] items-center justify-between pb-5 text-center'>
-        {isFavoriteCountWinner ? spacer : spacer}
-        <span
-          className={`text-h2-bold relative inline-flex items-center justify-center ${isFavoriteCountWinner ? 'h-6 rounded-full bg-orange-200 py-5' : ''}`}
-        >
-          {favoriteCount}개
+        {spacer}
+        <span className='relative inline-flex items-center justify-center'>
+          {isFavoriteCountWinner && (
+            <span className='absolute top-1/4 right-0 left-0 z-0 h-6 rounded-full bg-orange-200'></span>
+          )}
+          <span className='text-h2-bold relative z-10 px-4'>{favoriteCount}개</span>
         </span>
         {isFavoriteCountWinner ? winIcon : spacer}
       </div>
