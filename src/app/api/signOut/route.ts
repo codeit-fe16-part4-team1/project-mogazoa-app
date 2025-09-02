977f5d87abe531e1e28cf021ea28d4b3500d0ee8
Chiman2937 <cyga2933@gmail.com>
2025-09-02 14:50:17 +0900

    fix: 인증로직에서 loginType cookie 삭제, userId를 setcookie 해주도록 수정

--- src/app/api/signOut/route.ts
+++ src/app/api/signOut/route.ts
@@ -4,6 +4,6 @@ import { NextResponse } from 'next/server';
 export const POST = async () => {
   const res = NextResponse.json({ message: 'Signed out' }, { status: 200 });
   res.cookies.set('accessToken', '', EXPIRED_COOKIE_OPTIONS);
-  res.cookies.set('loginType', 'default', EXPIRED_COOKIE_OPTIONS);
+  res.cookies.set('userId', '0', EXPIRED_COOKIE_OPTIONS);
   return res;
 };
