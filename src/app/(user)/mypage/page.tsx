6c71c58a532e38b61bf78b5efc6506eea4e4514f
Chiman2937 <cyga2933@gmail.com>
2025-09-02 14:52:27 +0900

    fix: MyPage로 이동할 때 내 프로필 아이디를 받아오도록 수정(임시)

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -1,10 +1,12 @@
 import { getMyProfileAPI } from '@/api/user/getMyProfileAPI';
 import ProductSection from '../components/ProductSection';
 import ProfileSection from '../components/ProfileSection';
+import { getMyProfileId } from '@/lib/getMyProfileId';
 
 const MyPage = async () => {
   const profile = await getMyProfileAPI();
-
+  const myProfileId = await getMyProfileId();
+  console.log(`[DEBUG] My Profile Id: ${myProfileId}`);
   return (
     <div className='bg-gray-100'>
       <ProfileSection profile={profile} isMyProfile={true} />
