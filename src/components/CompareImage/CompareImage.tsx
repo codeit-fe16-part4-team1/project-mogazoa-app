d7ccae29e3eda98f2ee17e49f2db65ca5c375d08
qkrekdls <gunmannduu@gmail.com>
2025-09-02 16:33:02 +0900

    피드백 반영

--- src/components/CompareImage/CompareImage.tsx
+++ src/components/CompareImage/CompareImage.tsx
@@ -8,7 +8,7 @@ interface CompareImageProps {
 
 const CompareImage = ({ imageUrl, productName, placeholder }: CompareImageProps) => {
   return (
-    <div className='rounded-x3 md:rounded-x4 relative aspect-square h-45 w-25 overflow-hidden border border-gray-300 bg-gray-200 md:h-45 md:w-45'>
+    <div className='rounded-x3 md:rounded-x4 relative aspect-square w-25 overflow-hidden border border-gray-300 bg-gray-200 md:w-45'>
       {imageUrl ? (
         <Image
           src={imageUrl}
