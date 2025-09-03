3bc5a4949f67449f0f0eeacb472371a51addfab6
Chiman2937 <cyga2933@gmail.com>
2025-09-03 15:52:40 +0900

    refactor: 팔로우/언팔로우 state 관리 없이 임시쿼리로 낙관적 업데이트 적용되도록 수정

--- src/app/(user)/components/ProfileSection/ProfileButtonArea.tsx
+++ src/app/(user)/components/ProfileSection/ProfileButtonArea.tsx
@@ -1,9 +1,10 @@
 import { userFollowAPI } from '@/api/follow/userFollowAPI';
 import { userUnfollowAPI } from '@/api/follow/userUnfollowAPI';
 import { Button } from '@/components/Button/Button';
+import { getMyProfileId } from '@/lib/getMyProfileId';
 import { Profile } from '@/types/api';
-import { useMutation } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+// import { useState } from 'react';
 
 interface Props {
   profile: Profile;
@@ -11,7 +12,9 @@ interface Props {
 }
 
 const ProfileButtonArea = ({ profile, isMyProfile }: Props) => {
-  const [isFollowing, setIsFollowing] = useState(profile.isFollowing);
+  // const [isFollowing, setIsFollowing] = useState(profile.isFollowing);
+
+  const queryClient = useQueryClient();
 
   const { mutate } = useMutation({
     mutationFn: async (isFollowing: boolean) => {
@@ -21,22 +24,41 @@ const ProfileButtonArea = ({ profile, isMyProfile }: Props) => {
         await userFollowAPI({ userId: profile.id });
       }
     },
-    onMutate: (originalValue) => {
-      setIsFollowing(!originalValue);
-      return { originalValue };
+    onMutate: async (originalValue) => {
+      const previousProfile = queryClient.getQueryData(['profile', profile.id]);
+      // setIsFollowing(!originalValue);
+      queryClient.setQueryData(['profile', profile.id], (old) => {
+        if (!old) {
+          return {
+            ...profile,
+            isFollowing: !originalValue,
+            followersCount: originalValue ? profile.followersCount - 1 : profile.followersCount + 1,
+          };
+        }
+        return {
+          ...old,
+          isFollowing: !originalValue,
+          followersCount: originalValue ? profile.followersCount - 1 : profile.followersCount + 1,
+        };
+      });
+      return { previousProfile };
     },
     onError: (_error, _variables, context) => {
-      if (context?.originalValue !== undefined) {
-        setIsFollowing(context.originalValue);
+      if (context?.previousProfile !== undefined) {
+        // setIsFollowing(context.originalValue);
+        queryClient.setQueryData(['profile', profile.id], context.previousProfile);
       }
     },
-    // onSettled: () => {
-    //   queryClient.invalidateQueries({ queryKey: ['profile', profile.id] });
-    // },
+    onSettled: async () => {
+      queryClient.invalidateQueries({ queryKey: ['profile', profile.id] });
+      const myProfileId = await getMyProfileId();
+      if (!myProfileId) return;
+      queryClient.invalidateQueries({ queryKey: ['profile', myProfileId] });
+    },
   });
 
   const handleFollowToggle = () => {
-    mutate(isFollowing);
+    mutate(profile.isFollowing);
   };
 
   const BUTTON_STYLES = 'mx-auto block w-full md:h-15 lg:w-160';
@@ -51,11 +73,11 @@ const ProfileButtonArea = ({ profile, isMyProfile }: Props) => {
       {!isMyProfile && (
         <Button
           size='S'
-          intent={isFollowing ? 'secondary' : 'primary'}
+          intent={profile.isFollowing ? 'secondary' : 'primary'}
           className={BUTTON_STYLES}
           onClick={handleFollowToggle}
         >
-          {isFollowing ? '팔로우 취소' : '팔로우'}
+          {profile.isFollowing ? '팔로우 취소' : '팔로우'}
         </Button>
       )}
     </article>
