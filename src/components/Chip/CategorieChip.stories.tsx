ff57f954522919879ea72d1cea43ad3b8545a589
Heedong <donghui11777@gmail.com>
2025-09-06 21:43:54 +0900

    fix: queryClient 로직 수정

--- src/components/Chip/CategorieChip.stories.tsx
+++ src/components/Chip/CategorieChip.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj, StoryFn } from '@storybook/nextjs-vite';
 
 import { CategorieChip } from './CategorieChip';
+import { useCategoryMap } from '@/hooks/useCategoryMap';
 
 const meta: Meta<typeof CategorieChip> = {
   title: 'Components/CategorieChip',
@@ -11,30 +12,14 @@ const meta: Meta<typeof CategorieChip> = {
 export default meta;
 type Story = StoryObj<typeof CategorieChip>;
 
-// Template 함수의 타입을 StoryFn으로 명시하고 args를 받도록 합니다.
 const Template: StoryFn<typeof CategorieChip> = (args) => {
-  const categories = [
-    '음악',
-    '영화/드라마',
-    '강의/책',
-    '호텔',
-    '가구/인테리어',
-    '식당',
-    '전자기기',
-    '화장품',
-    '의류/악세서리',
-    '앱',
-    '여행',
-    '테스트텍스트',
-    'test text',
-    'Test Text',
-  ];
+  const { categoryData } = useCategoryMap();
+  console.log(categoryData);
 
   return (
     <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px' }}>
-      {categories.map((category, index) => (
-        // {...args}를 통해 variant prop이 전달됩니다.
-        <CategorieChip key={category} {...args} categoryId={index + 1} />
+      {categoryData?.map((category) => (
+        <CategorieChip key={category.id} {...args} categoryId={category.id} />
       ))}
     </div>
   );
