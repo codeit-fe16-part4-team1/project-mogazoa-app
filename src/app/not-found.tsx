5a9200bf808e74ca9cc9cde5c5a2e1ce23dba14d
mingyeong <primg67@naver.com>
2025-09-06 16:38:02 +0900

    feat: 로딩, not-found 페이지

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,30 @@
-const NotFound = () => {
+'use client';
+import { Button } from '@/components/Button/Button';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
+
+const NotFoundPage = () => {
+  const router = useRouter();
+
+  const handleHomeRedirectClick = () => {
+    router.replace('/');
+  };
+
   return (
-    <div>
-      <h1>페이지를 찾을 수 없습니다</h1>
-    </div>
+    <section className='fullscreen flex items-center justify-center'>
+      <div className='layout-center'>
+        <div className='relative mb-5 aspect-square w-50 md:w-70'>
+          <Image fill src='/images/image_error.png' alt='error' className='object-cover' />
+        </div>
+        <h1 className='text-h3-bold md:text-h1-bold font-cafe24-supermagic mb-10 text-gray-800'>
+          페이지를 찾을 수 없습니다.
+        </h1>
+        <Button intent='tertiary' className='w-full md:h-16' onClick={handleHomeRedirectClick}>
+          홈으로 가기
+        </Button>
+      </div>
+    </section>
   );
 };
 
-export default NotFound;
+export default NotFoundPage;
