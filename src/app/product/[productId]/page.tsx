9f56254d1cfaf59f472db10c3646eece95f81b0f
Heedong <donghui11777@gmail.com>
2025-09-08 15:32:39 +0900

    feat: ProductDetailPage generateMetadata 함수 추가로 동적 SEO 생성

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -4,6 +4,78 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import { productKeys, reviewKeys } from '@/constant/queryKeys';
 import ProductDetailPage from './ProductDetailPage';
 import { getUserInfo } from '@/lib/getUserInfo';
+import { Metadata } from 'next';
+import { headers } from 'next/headers';
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { productId: string };
+}): Promise<Metadata> {
+  const productId = Number(params.productId);
+
+  const headersList = headers();
+  const host = (await headersList).get('host') || 'new-project-final.link';
+  const currentUrl = `https://${host}/product/${productId}`;
+
+  try {
+    const productDetail = await getProductDetail(productId);
+
+    return {
+      title: `${productDetail.name} - 상품 상세`,
+      description: productDetail.description,
+      keywords: [
+        'mogazoa',
+        '모가조아',
+        productDetail.name,
+        '상품평가',
+        '상품통계',
+        '상품비교',
+        '상품리뷰',
+      ],
+      openGraph: {
+        title: `${productDetail.name} - 상품 상세 | mogazoa`,
+        description: productDetail.description,
+        siteName: 'mogazoa',
+        locale: 'ko_KR',
+        type: 'article',
+        publishedTime: productDetail.createdAt,
+        modifiedTime: productDetail.updatedAt,
+        section: productDetail.category.name,
+        url: currentUrl,
+        images: [
+          {
+            url: productDetail.image,
+            width: 200,
+            height: 200,
+            alt: `${productDetail.name} 상품 이미지`,
+          },
+        ],
+      },
+      robots: {
+        index: true,
+        follow: true,
+      },
+      alternates: {
+        canonical: currentUrl,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch product detail for metadata:', error);
+    return {
+      title: '상품 상세 페이지',
+      description: '상품 정보를 확인하세요.\n\nmogazoa에서 다양한 상품 리뷰와 정보를 확인하세요',
+      openGraph: {
+        title: `상품 상세`,
+        description: '상품 정보를 확인하세요.\n\nmogazoa에서 다양한 상품 리뷰와 정보를 확인하세요',
+        siteName: 'mogazoa',
+        locale: 'ko_KR',
+        type: 'article',
+        url: currentUrl,
+      },
+    };
+  }
+}
 
 const ProductDetailPageServer = async ({ params }: { params: Promise<{ productId: string }> }) => {
   const { productId: slug } = await params;
