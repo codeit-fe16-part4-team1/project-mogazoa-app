8d8bfb3cadc4b11459132617fe58cec0b93ebad6
mingyeong <primg67@naver.com>
2025-09-08 13:10:32 +0900

    refactor: tailwind 애니메이션 적용

--- src/app/loading/loading.tsx
+++ src/app/loading/loading.tsx
@@ -4,7 +4,7 @@ import IconLogo from '@/assets/icons/logo.svg';
 const Loading = () => {
   return (
     <section className='fullscreen layout-center flex flex-col items-center justify-center gap-6'>
-      <IconLogo className='animate-reveal h-15 w-60' />
+      <IconLogo className='h-15 w-60 animate-pulse' />
     </section>
   );
 };
