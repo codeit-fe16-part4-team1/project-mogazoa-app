50a5452811a7b80a27cc30186a7ee6fd8a94493f
Chiman2937 <cyga2933@gmail.com>
2025-09-08 23:42:03 +0900

    feat: ProductCard Skeleton UI 적용

--- src/app/components/ProductCard/ProductCard.tsx
+++ src/app/components/ProductCard/ProductCard.tsx
@@ -15,18 +15,28 @@ interface ProductCardProps {
   linkToDetailPage?: boolean;
 }
 
-const PRODUCT_NAME_STYLES = [
-  'text-gray-900',
-  'text-body2-medium',
+const CONTAINER_STYLES = 'flex cursor-pointer flex-col gap-3 md:gap-5';
+
+const IMAGE_WRAPPER_STYLES =
+  'relative aspect-square w-full overflow-hidden rounded-xl border-1 border-gray-300 md:aspect-[300/276]';
+
+const CONTENTS_STYLES = 'flex flex-col gap-2 md:gap-3 md:px-2';
+
+const NAME_STYLES = clsx(
+  'text-gray-900 text-body2-medium md:text-sub-headline-medium w-full',
   'tracking-[-0.4px]',
-  'md:text-sub-headline-medium',
-];
-const PRODUCT_STATS_STYLES = [
-  'text-caption',
-  'text-gray-700',
-  'tracking-[-0.4px]',
-  'md:text-body1',
-];
+);
+
+const STATS_CONTAINER_STYLES = 'flex gap-2';
+
+const STATS_STYLES = clsx('text-caption md:text-body1 text-gray-700', 'tracking-[-0.4px]');
+
+//skeleton styles
+const NAME_SKELETON_STYLES =
+  'h-body2-skeleton md:h-sub-headline-skeleton skeleton-animate rounded-md w-3/5';
+
+const STATS_SKELETON_STYLES =
+  'h-caption-skeleton md:h-body1-skeleton skeleton-animate w-10 rounded-md';
 
 const ProductCard = ({
   id,
@@ -46,8 +56,8 @@ const ProductCard = ({
     }
   };
   return (
-    <div className='flex cursor-pointer flex-col gap-3 md:gap-5' onClick={handleClick}>
-      <div className='relative aspect-square w-full overflow-hidden rounded-xl border-1 border-gray-300 md:aspect-[300/276]'>
+    <div className={CONTAINER_STYLES} onClick={handleClick}>
+      <div className={IMAGE_WRAPPER_STYLES}>
         <Image
           src={safeImageUrl}
           sizes='(max-width: 768px) 50vw, 33vw'
@@ -64,18 +74,18 @@ const ProductCard = ({
           onError={onError}
         />
       </div>
-      <div className='flex flex-col gap-2 md:gap-3 md:px-2'>
-        <div className={clsx('w-full', ...PRODUCT_NAME_STYLES)}>{name}</div>
-        <div className={clsx('flex gap-2', ...PRODUCT_STATS_STYLES)}>
-          <div className='flex gap-1'>
+      <div className={CONTENTS_STYLES}>
+        <div className={NAME_STYLES}>{name}</div>
+        <div className={STATS_CONTAINER_STYLES}>
+          <div className={STATS_STYLES}>
             <span>리뷰</span>
             <span>{reviewCount}</span>
           </div>
-          <div className='flex gap-1'>
+          <div className={STATS_STYLES}>
             <span>찜</span>
             <span>{likeCount}</span>
           </div>
-          <div className='ml-auto flex gap-1'>
+          <div className={clsx(STATS_STYLES, 'ml-auto')}>
             <span>⭐</span>
             <span>{rating.toFixed(1)}</span>
           </div>
@@ -84,5 +94,23 @@ const ProductCard = ({
     </div>
   );
 };
+
+const Skeleton = () => {
+  return (
+    <div className={CONTAINER_STYLES}>
+      <div className={IMAGE_WRAPPER_STYLES} />
+      <div className={CONTENTS_STYLES}>
+        <div className={NAME_SKELETON_STYLES} />
+        <div className={STATS_CONTAINER_STYLES}>
+          <div className={STATS_SKELETON_STYLES} />
+          <div className={STATS_SKELETON_STYLES} />
+          <div className={clsx(STATS_SKELETON_STYLES, 'ml-auto')} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ProductCard.skeleton = Skeleton;
 
 export default ProductCard;
