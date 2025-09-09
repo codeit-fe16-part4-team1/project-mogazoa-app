6e2d96b4ebcd122d72637dc55fbf176cb5f89d76
dain <gunmannduu@gmail.com>
2025-09-09 17:36:42 +0900

    fix: zustand-middleware사용

--- src/store/useCompareStore.ts
+++ src/store/useCompareStore.ts
@@ -2,6 +2,7 @@
 
 import { create } from 'zustand';
 import { ProductItem } from '@/types/api';
+import { persist } from 'zustand/middleware';
 
 interface CompareStore {
   products: ProductItem[];
@@ -11,42 +12,29 @@ interface CompareStore {
   resetProducts: () => void;
 }
 
-const STORAGE_KEY = 'compareProducts';
-
-export const useCompareStore = create<CompareStore>((set, get) => ({
-  products: [],
-  addProduct: (product) => {
-    const state = get();
-    // 상품이 이미 있거나, 목록이 2개면 추가하지 않고 바로 반환
-    if (state.products.some((p) => p.id === product.id) || state.products.length >= 2) {
-      return;
-    }
-    set({ products: [...state.products, product] });
-  },
-  setComparisonProducts: (newProducts) => {
-    set({ products: newProducts });
-  },
-  removeProduct: (productId) => {
-    set((state) => ({ products: state.products.filter((p) => p.id !== productId) }));
-  },
-  resetProducts: () => {
-    set({ products: [] });
-  },
-}));
-
-let isInitialized = false;
-
-useCompareStore.subscribe((state) => {
-  try {
-    if (!isInitialized) {
-      const storedProducts = localStorage.getItem(STORAGE_KEY);
-      if (storedProducts) {
-        useCompareStore.setState({ products: JSON.parse(storedProducts) });
-      }
-      isInitialized = true;
-    }
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(state.products));
-  } catch (e) {
-    console.error('localStorage 동기화 오류', e);
-  }
-});
+export const useCompareStore = create<CompareStore>()(
+  persist(
+    (set, get) => ({
+      products: [],
+      addProduct: (product) => {
+        const state = get();
+        if (state.products.some((p) => p.id === product.id) || state.products.length >= 2) {
+          return;
+        }
+        set({ products: [...state.products, product] });
+      },
+      setComparisonProducts: (newProducts) => {
+        set({ products: newProducts });
+      },
+      removeProduct: (productId) => {
+        set((state) => ({ products: state.products.filter((p) => p.id !== productId) }));
+      },
+      resetProducts: () => {
+        set({ products: [] });
+      },
+    }),
+    {
+      name: 'compareProducts',
+    },
+  ),
+);
