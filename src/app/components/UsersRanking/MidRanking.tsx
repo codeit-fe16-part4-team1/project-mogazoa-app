53b0abd3d0bafb5004812317a25c3f189438029d
mingyeong <primg67@naver.com>
2025-09-09 20:19:43 +0900

    feat: 리뷰어랭킹에서 유저 선택시 유저 프로필로 이동

--- src/app/components/UsersRanking/MidRanking.tsx
+++ src/app/components/UsersRanking/MidRanking.tsx
@@ -1,5 +1,6 @@
 import { UserRanking } from '@/types/api';
 import { cn } from '@/lib/cn';
+import { useRouter } from 'next/navigation';
 
 const MidRanking = ({
   user,
@@ -10,13 +11,21 @@ const MidRanking = ({
   index: number;
   className: string;
 }) => {
+  const router = useRouter();
   return (
     <div className={cn('flex items-center justify-between tracking-[-0.4px]', className)}>
       <div className='flex items-center gap-2'>
         <span className='text-caption-medium rounded-full bg-gray-200 px-2 py-1 text-gray-700'>
           {index + 4}등
         </span>
-        <span className='md:text-body1 text-body2 text-gray-900'>{user.nickname}</span>
+        <span
+          className='md:text-body1 text-body2 cursor-pointer text-gray-900 hover:underline hover:underline-offset-2'
+          onClick={() => {
+            router.push(`/user/${user.id}`);
+          }}
+        >
+          {user.nickname}
+        </span>
       </div>
       <div className='text-caption flex gap-3 text-right text-gray-500'>
         <div className='followers flex gap-0.5 md:gap-1'>
