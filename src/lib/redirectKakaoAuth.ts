7096626c0c203997b03098dcb924c6a6085bcd95
Chiman2937 <cyga2933@gmail.com>
2025-09-09 13:24:01 +0900

    fix: redirectKakaoAuth.ts 원복, type 삭제

--- src/lib/redirectKakaoAuth.ts
+++ src/lib/redirectKakaoAuth.ts
@@ -1,4 +1,17 @@
-export const redirectKakaoAuth = () => {
+export const redirectKakaoAuth = {
+  signin: () => {
+    redirect({});
+  },
+  signup: ({ state }: { state: string }) => {
+    redirect({ state });
+  },
+};
+
+interface RedirectFunctionType {
+  state?: string;
+}
+
+const redirect = ({ state }: RedirectFunctionType) => {
   const redirectUri = `${window.location.origin}/oauth/signup/kakao`;
   if (process.env.NODE_ENV === 'development') {
     const clientId = process.env.NEXT_PUBLIC_JS_KEY_KAKAO; // 환경변수에서
@@ -8,12 +21,14 @@ export const redirectKakaoAuth = () => {
       `client_id=${clientId}&` +
       `redirect_uri=${encodeURIComponent(redirectUri)}&` +
       `response_type=code&` +
+      `${state ? `state=${encodeURIComponent(state)}&` : ''}` +
       `prompt=login`;
 
     window.location.href = kakaoAuthUrl;
   } else if (process.env.NODE_ENV === 'production') {
     Kakao.Auth.authorize({
       redirectUri,
+      ...(state && { state }),
     });
   }
 };
