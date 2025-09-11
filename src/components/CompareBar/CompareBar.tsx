f8a4c507d72244a94e16a23547c07c991d7da8c6
dain <gunmannduu@gmail.com>
2025-09-11 20:56:41 +0900

    fix: 상품입력바 x버튼 cursor-pointer 추가

--- src/components/CompareBar/CompareBar.tsx
+++ src/components/CompareBar/CompareBar.tsx
@@ -77,7 +77,7 @@ const CompareBar = ({
       {selectedProduct ? (
         <div className='inline-flex w-full items-center justify-between gap-3 rounded-full bg-gray-900 px-5 py-[18px] text-white'>
           <span className='text-body1-bold'>{selectedProduct.name}</span>
-          <button onClick={handleRemove} className='items-center justify-between'>
+          <button onClick={handleRemove} className='cursor-pointer items-center justify-between'>
             <IconClose className='h-5 w-5' />
           </button>
         </div>
