84f434b361975e16d0b48dd5bc0a9174ba119392
Chiman2937 <cyga2933@gmail.com>
2025-09-12 12:57:42 +0900

    fix: TextArea 컴포넌트 - required 속성 추가

--- src/lib/validations.ts
+++ src/lib/validations.ts
@@ -4,12 +4,19 @@ import { z } from 'zod';
 interface TextArea {
   minLength?: number;
   maxLength: number;
+  required?: boolean;
 }
 
-export const TextAreaSchema = ({ minLength = 1, maxLength }: TextArea) => {
-  return z
-    .string()
-    .min(1, '필수 입력 항목입니다') // min은 고정
-    .min(minLength, `${minLength}자 이상으로 입력하세요`)
-    .max(maxLength, `${maxLength}자 이하로 입력하세요`);
+export const TextAreaSchema = ({ minLength = 1, maxLength, required = true }: TextArea) => {
+  if (required) {
+    return z
+      .string()
+      .min(minLength, `${minLength}자 이상으로 입력하세요`)
+      .max(maxLength, `${maxLength}자 이하로 입력하세요`);
+  } else {
+    return z
+      .string() //
+      .max(maxLength, `${maxLength}자 이하로 입력하세요`)
+      .optional(); // 필수가 아님
+  }
 };
