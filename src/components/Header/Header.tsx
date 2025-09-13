a96b015b1b6da86e80cdc515c4be89f5947bf24c
mingyeong <primg67@naver.com>
2025-09-13 13:56:35 +0900

    refactor: 링크 선택해도 검색창 움직이지 않게

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,18 +11,14 @@ import SearchInput from './SearchInput';
 import { useCompareStore } from '@/store/useCompareStore';
 
 const HEADER_LINK_STYLES =
-  'text-body2 hidden py-2 text-gray-700 hover:text-gray-800 md:block lg:text-body1 whitespace-nowrap';
+  'text-body2 hidden py-2 text-gray-700 hover:text-gray-800 md:block lg:text-body1 whitespace-nowrap text-center';
 
 const HEADER_ACTIVE_LINK_STYLES =
-  'text-body2-bold border-primary-orange-500 text-primary-orange-600 hover:text-border-primary-orange-500 lg:text-body1-bold hidden rounded-full border-1 px-4 py-3 transition-colors duration-300 ease-in-out md:block';
+  'text-body2-bold border-primary-orange-500 text-primary-orange-600 hover:text-border-primary-orange-500 lg:text-body1-bold hidden rounded-full border-1 px-4 py-3 transition-colors duration-300 ease-in-out md:block text-center';
 
 const Header = () => {
   const { isAuthenticated } = useAuthStore();
   const pathname = usePathname();
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isSearchOpen, setIsSearchOpen] = useState(false);
-  const menuRef = useRef<HTMLDivElement>(null);
-  const searchRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
   const searchParams = useSearchParams();
   const searchQuery = searchParams.get('query') || '';
@@ -80,37 +76,12 @@ const Header = () => {
     setLocalSearchQuery(inputValue);
   };
 
-  useEffect(() => {
-    const handleClickOutside = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        setIsMenuOpen(false);
-      }
-
-      if (
-        !localSearchQuery &&
-        searchRef.current &&
-        !searchRef.current.contains(event.target as Node)
-      ) {
-        setIsSearchOpen(false);
-      }
-    };
-
-    if (isMenuOpen || isSearchOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    }
-
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [isMenuOpen, isSearchOpen, localSearchQuery]);
-
   const linkText = compareCount > 0 ? `비교하기 (${compareCount}/2)` : '비교하기';
 
   return (
     <header className={clsx('z-header sticky top-0 border-b-1 border-gray-200 bg-white', 'header')}>
       <div className='mx-auto max-w-[1680px] px-5 md:px-8'>
         <div className='flex h-16 w-full items-center md:h-20 md:justify-between lg:h-[100px]'>
-          {/* <div className='flex items-center'> */}
           <div
             className='hidden h-8 w-32 cursor-pointer md:block md:h-10 md:w-40'
             onClick={handleLogoClick}
@@ -120,7 +91,6 @@ const Header = () => {
           <div className='mr-5 size-8 cursor-pointer md:hidden' onClick={handleLogoClick}>
             <IconLogoMobile className='h-full w-full' />
           </div>
-          {/* </div> */}
           <div className='flex flex-1 items-center md:flex-none md:gap-4 lg:gap-10'>
             <form
               className='flex h-12 flex-1 items-center md:h-12.5 md:min-w-75 md:flex-none lg:h-14 lg:w-125'
@@ -141,6 +111,7 @@ const Header = () => {
               className={cn(
                 HEADER_LINK_STYLES,
                 pathname === '/compare' ? HEADER_ACTIVE_LINK_STYLES : '',
+                compareCount > 0 ? 'w-34' : 'w-24',
               )}
             >
               {linkText}
@@ -152,6 +123,7 @@ const Header = () => {
                   className={cn(
                     HEADER_LINK_STYLES,
                     pathname === '/signin' ? HEADER_ACTIVE_LINK_STYLES : '',
+                    'w-20',
                   )}
                 >
                   로그인
@@ -164,6 +136,7 @@ const Header = () => {
                   className={cn(
                     HEADER_LINK_STYLES,
                     pathname === '/mypage' ? HEADER_ACTIVE_LINK_STYLES : '',
+                    'w-24.5',
                   )}
                 >
                   내 프로필
