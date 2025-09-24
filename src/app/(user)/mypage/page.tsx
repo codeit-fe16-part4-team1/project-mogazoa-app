37a7550f39743f392b56f398cd1a245abbf1c8f6
Chiman2937 <cyga2933@gmail.com>
2025-09-24 21:54:18 +0900

    fix: 프로필 페이지 - api 호출 구문 통합

--- src/app/(user)/mypage/page.tsx
+++ src/app/(user)/mypage/page.tsx
@@ -1,43 +1,11 @@
-import { getMyProfileAPI } from '@/api/user/getMyProfileAPI';
-import ProductSection from '../components/ProductSection';
 import { getUserInfo } from '@/lib/getUserInfo';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import ProfileSection from '../components/ProfileSection';
-import { productKeys, profileKeys } from '@/constant/queryKeys';
-import { getUserProductsAPI } from '@/api/user/getUserProductsAPI';
 import { redirect } from 'next/navigation';
+import ProfilePage from '../ProfilePage';
 
 const MyPage = async () => {
-  const queryClient = new QueryClient();
-  const { userId } = await getUserInfo();
-  if (!userId) redirect('/signin');
-  console.log(`[DEBUG] My Profile Id: ${userId}`);
-
-  await Promise.all([
-    queryClient.fetchQuery({
-      queryKey: profileKeys.detail(userId),
-      queryFn: () => getMyProfileAPI(),
-    }),
-    queryClient.fetchInfiniteQuery({
-      queryKey: productKeys.userProductList(userId, 'reviewed'),
-      queryFn: ({ pageParam }) =>
-        getUserProductsAPI({
-          userId: userId,
-          type: 'reviewed',
-          cursor: pageParam || 0,
-        }),
-      initialPageParam: 0,
-      getNextPageParam: (lastPage) => lastPage.nextCursor ?? undefined,
-      pages: 0,
-    }),
-  ]);
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfileSection profileId={userId} isMyProfile={true} />
-      <ProductSection profileId={userId} />
-    </HydrationBoundary>
-  );
+  const { userId: myUserId } = await getUserInfo();
+  if (!myUserId) redirect('/signin');
+  return <ProfilePage profileId={myUserId} isMyProfile={true} />;
 };
 
 export default MyPage;
--- src/app/(user)/user/[userId]/page.tsx
+++ src/app/(user)/user/[userId]/page.tsx
@@ -1,12 +1,8 @@
-import ProfileSection from '../../components/ProfileSection';
-import ProductSection from '../../components/ProductSection';
 import { getUserInfo } from '@/lib/getUserInfo';
 import { redirect } from 'next/navigation';
-import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
-import { getUserProfileAPI } from '@/api/user/getUserProfileAPI';
-import { profileKeys } from '@/constant/queryKeys';
 import { Metadata } from 'next';
 import { generateProfileMetadata } from '@/lib/metadata/profile';
+import ProfilePage from '../../ProfilePage';
 
 export interface ProfilePageProps {
   params: Promise<{
@@ -20,28 +16,11 @@ export const generateMetadata = async ({ params }: ProfilePageProps): Promise<Me
 };
 
 const UserPage = async ({ params }: ProfilePageProps) => {
-  const queryClient = new QueryClient();
-
   const { userId } = await params;
+  const { userId: myUserId } = await getUserInfo();
   const profileId = Number(userId);
-
-  const { userId: myProfileId } = await getUserInfo();
-
-  if (profileId === myProfileId) redirect('/mypage');
-
-  console.log(`[DEBUG] User Profile Id: ${profileId}`);
-
-  await queryClient.fetchQuery({
-    queryKey: profileKeys.detail(profileId),
-    queryFn: () => getUserProfileAPI({ userId: profileId }),
-  });
-
-  return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
-      <ProfileSection profileId={profileId} isMyProfile={false} />
-      <ProductSection profileId={profileId} />
-    </HydrationBoundary>
-  );
+  if (profileId === myUserId) redirect('/mypage');
+  return <ProfilePage profileId={profileId} isMyProfile={false} />;
 };
 
 export default UserPage;
