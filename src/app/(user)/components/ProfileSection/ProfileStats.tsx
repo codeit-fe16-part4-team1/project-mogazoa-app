abadc36dfdf8dca0b32de0018cad7aaa2e674375
Chiman2937 <cyga2933@gmail.com>
2025-09-24 21:53:29 +0900

    feat: CategoryChip 새로 제작

--- src/app/(user)/components/ProfileSection/ProfileStats.tsx
+++ src/app/(user)/components/ProfileSection/ProfileStats.tsx
@@ -1,4 +1,4 @@
-import { CategorieChip } from '@/components/Chip/CategorieChip';
+import CategoryChip from '@/components/Chip/CategoryChip';
 import { Profile } from '@/types/api';
 
 interface Props {
@@ -10,25 +10,25 @@ const ProfileStats = ({ profile }: Props) => {
     {
       label: '남긴 별점 평균',
       value: (
-        <p className='text-sub-headline-bold px-2 py-1 md:text-2xl'>
+        <span className='text-sub-headline-bold md:text-2xl'>
           {profile.averageRating.toFixed(1)}
-        </p>
+        </span>
       ),
     },
     {
       label: '남긴 리뷰',
-      value: <p className='text-sub-headline-bold px-2 py-1 md:text-2xl'>{profile.reviewCount}</p>,
+      value: <span className='text-sub-headline-bold md:text-2xl'>{profile.reviewCount}</span>,
     },
     {
       label: '관심 카테고리',
       value: profile?.mostFavoriteCategory ? (
-        <CategorieChip
-          key={profile.mostFavoriteCategory?.name}
-          categoryId={profile.mostFavoriteCategory.id}
-          variant='default'
+        <CategoryChip
+          categoryId={profile?.mostFavoriteCategory.id}
+          className='flex flex-row items-center gap-1'
         >
-          {profile.mostFavoriteCategory?.name}
-        </CategorieChip>
+          <CategoryChip.text className='text-body1-bold md:text-h4-bold text-gray-900' />
+          <CategoryChip.icon className='size-5 text-gray-800 md:size-7' />
+        </CategoryChip>
       ) : (
         <p className='text-sub-headline-bold px-2 py-1 md:text-2xl'>-</p>
       ),
@@ -39,7 +39,7 @@ const ProfileStats = ({ profile }: Props) => {
     <article className='mx-4 mb-10 flex flex-row justify-between border-t-1 border-gray-200 pt-7 md:mx-2 md:mb-12 md:justify-around lg:mx-auto lg:mb-9 lg:max-w-170'>
       {ListMap.map(({ label, value }) => (
         <div key={label} className='flex flex-col items-center justify-between gap-2'>
-          {value}
+          <div className='flex items-center'>{value}</div>
           <p className='text-body2 md:text-body1 text-gray-600'>{label}</p>
         </div>
       ))}
---
+++ src/components/Chip/CategoryChip.tsx
@@ -0,0 +1,49 @@
+import { categoryList } from '@/lib/CategoryList';
+import { createContext, useContext } from 'react';
+
+interface CategoryContextType {
+  categoryId: number;
+}
+
+const CategoryContext = createContext<CategoryContextType>({
+  categoryId: 0,
+});
+
+interface CategoryProps {
+  categoryId: number;
+  className?: string;
+  children: React.ReactNode;
+}
+
+const CategoryChip = ({ categoryId, className, children }: CategoryProps) => {
+  return (
+    <CategoryContext.Provider value={{ categoryId }}>
+      <div className={className}>{children}</div>
+    </CategoryContext.Provider>
+  );
+};
+
+interface IconProps {
+  className?: string;
+}
+
+const CategoryIcon = ({ className }: IconProps) => {
+  const { categoryId } = useContext(CategoryContext);
+  const IconComponent = categoryList.find((c) => c.id === categoryId)?.icon;
+  return <IconComponent className={className} />;
+};
+
+interface TextProps {
+  className?: string;
+}
+
+const CategoryText = ({ className }: TextProps) => {
+  const { categoryId } = useContext(CategoryContext);
+  const categoryName = categoryList.find((c) => c.id === categoryId)?.name;
+  return <span className={className}>{categoryName}</span>;
+};
+
+CategoryChip.icon = CategoryIcon;
+CategoryChip.text = CategoryText;
+
+export default CategoryChip;
