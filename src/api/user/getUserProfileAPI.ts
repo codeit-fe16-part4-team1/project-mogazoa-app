57ecc0b2875104ad33cf9207d080332a705e3b7b
Chiman2937 <cyga2933@gmail.com>
2025-09-24 18:51:27 +0900

    fix: 프로필페이지 NaN 오류 처리를 getUserProfileAPI로 이관

--- src/api/user/getUserProfileAPI.ts
+++ src/api/user/getUserProfileAPI.ts
@@ -1,5 +1,6 @@
 import { baseAPI } from '@/lib/baseAPI';
 import { Profile } from '@/types/api';
+import { notFound } from 'next/navigation';
 
 interface GetUserProfilePayload {
   userId: number;
@@ -7,6 +8,9 @@ interface GetUserProfilePayload {
 
 export const getUserProfileAPI = async (payload: GetUserProfilePayload): Promise<Profile> => {
   const { userId } = payload;
+  if (isNaN(userId)) {
+    notFound();
+  }
   const res = await baseAPI.get(`/users/${userId}`);
   return res.data;
 };
