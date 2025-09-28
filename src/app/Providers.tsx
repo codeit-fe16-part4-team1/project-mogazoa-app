c2b9d505ed4f529fe80cee6d16305435aade3a3c
Chiman2937 <cyga2933@gmail.com>
2025-09-28 17:53:28 +0900

    test: kakao script 제거 LCP test

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,7 +1,7 @@
 'use client';
-import { KakaoProvider } from '@/providers/KakaoProvider';
+// import { KakaoProvider } from '@/providers/KakaoProvider';
 import { QueryProvider } from '@/providers/QueryProvider';
-import { UserProvider } from '@/providers/UserProvider';
+// import { UserProvider } from '@/providers/UserProvider';
 
 interface Props {
   children: React.ReactNode;
@@ -10,9 +10,11 @@ interface Props {
 export const Providers = ({ children }: Props) => {
   return (
     <QueryProvider>
-      <KakaoProvider>
-        <UserProvider>{children}</UserProvider>
-      </KakaoProvider>
+      {/* <KakaoProvider> */}
+      {/* <UserProvider> */}
+      {children}
+      {/* </UserProvider> */}
+      {/* </KakaoProvider> */}
     </QueryProvider>
   );
 };
