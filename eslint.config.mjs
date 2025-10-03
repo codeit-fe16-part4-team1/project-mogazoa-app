b646d6a3f31a8c4519212f04da8865442ff35a90
Chiman2937 <cyga2933@gmail.com>
2025-10-03 14:50:38 +0900

    feat: eslint config - import 정렬 순서 옵션 추가

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
+import { FlatCompat } from '@eslint/eslintrc';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import storybook from 'eslint-plugin-storybook';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,14 +12,45 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends('next/core-web-vitals', 'next/typescript'), {
-  rules: {
-    'no-unused-vars': 'off', // JS용 기본 비활성화
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
-    ],
+const eslintConfig = [
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'no-unused-vars': 'off', // JS용 기본 비활성화
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            // CSS imports
+            ['\\.css$'],
+            // Next.js (일반 import)
+            ['^next(?!.*type)'],
+            // Next.js (type import)
+            ['^next.*\\u0000$'],
+            // React (일반 import)
+            ['^react(?!.*type)'],
+            // React (type import)
+            ['^react.*\\u0000$'],
+            // 서드파티 (외부 라이브러리)
+            ['^@?\\w'],
+            // 로컬 파일 (@/ 경로)
+            ['^@/'],
+            // 상대 경로
+            ['^\\.'],
+          ],
+        },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'simple-import-sort/exports': 'error',
+    },
   },
-}, ...storybook.configs["flat/recommended"]];
+  ...storybook.configs['flat/recommended'],
+];
 
 export default eslintConfig;
